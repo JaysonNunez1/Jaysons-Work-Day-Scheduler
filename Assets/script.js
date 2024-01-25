aa6fd60f2dadad3e9991ddac70c121064f57f293
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-01-25 17:00:52 -0500

    added functions variables and conditional statements to javascript

---
+++ Assets/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+})
+
+  
+  const currentDay = $('#currentDay');
+  const HourContainers = $('div[id^="hour"]');
+  const SaveButton = $('.saveBtn');
+  const today = dayjs();
+  
+function LoadsFromLocalStorage(){
+  HourContainers.each(function(){
+    const hourDivID = $(this).attr('id');
+    const EventName = localStorage.getItem(hourDivID);
+    $(this).find('textarea').val(EventName);
+  })
+}
+
+function SaveToLocalStorage () {
+  SaveButton.click(function(){
+    const hourDivID = $(this).closest('div').attr('id');
+    const textInput = $(this).siblings('textarea');
+    const eventName = textInput.val().trim();
+    localStorage.setItem(hourDivID,eventName);
+  })
+}
+
+if (eventName !== '') {
+  alert(eventName + 'successfully saved to schedule.');
+} else {
+  alert('Event/appointment successfully cleared from schedule.')
+}
+
+function applyTimeFormatting(){
+  HourContainers.each(function(){
+    const classHour = $(this).attr('id').slice(-2);
+    const currentHour = today.format('HH');
+    if (classHour < currentHour){
+      $(this).addClass('past');
+    } else if (classHour === currentHour){
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
+}
+
+LoadsFromLocalStorage();
+SaveToLocalStorage();
+applyTimeFormatting();
+  
+  
