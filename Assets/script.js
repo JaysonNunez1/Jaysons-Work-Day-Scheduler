096594fca55f348e2e45ed126890d187b6209282
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-01-25 22:17:39 -0500

    organized javascript file and extended container

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
 })
 
-  
-  const currentDay = $('#currentDay');
-  const HourContainers = $('div[id^="hour"]');
-  const SaveButton = $('.saveBtn');
-  const today = dayjs();
-  
+const currentDay = $('#currentDay');
+const HourContainers = $('div[id^="hour"]');
+const SaveButton = $('.saveBtn');
+const today = dayjs();
+
+currentDay.text(today.format('dddd,MMMM D'));
+
 function LoadsFromLocalStorage(){
-  HourContainers.each(function(){
+  hourContainers.each(function(){
     const hourDivID = $(this).attr('id');
     const EventName = localStorage.getItem(hourDivID);
     $(this).find('textarea').val(EventName);
@@ -30,6 +31,12 @@ if (eventName !== '') {
   alert('Event/appointment successfully cleared from schedule.')
 }
 
+
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+  
+
+
+
 function applyTimeFormatting(){
   HourContainers.each(function(){
     const classHour = $(this).attr('id').slice(-2);
@@ -43,6 +50,7 @@ function applyTimeFormatting(){
     }
   });
 }
+
 
 LoadsFromLocalStorage();
 SaveToLocalStorage();
